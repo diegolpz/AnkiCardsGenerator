5c78d499c1bfc748a2d472f2d7a8d36bb534ddc8
diegolpz <inglpz92@gmail.com>
2020-05-19 16:50:33 -0500

    initial commit

---
+++ scripts/AudioScrapper.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import re
+from bs4 import BeautifulSoup
+
+with open("test1.html") as fp:
+    soup = BeautifulSoup(fp,'html.parser')
+
+textOfTableIWant = soup.find('source')
+print(textOfTableIWant['src'])
---
+++ scripts/TableScrapper.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import re
+from bs4 import BeautifulSoup
+
+with open("test.html") as fp:
+    soup = BeautifulSoup(fp,'html.parser')
+
+textOfTableIWant = soup.find(string=re.compile("([Ii]nfinitive|[Nn]ominative)"))
+tableIWant = textOfTableIWant.find_parent("table")
+for child in tableIWant.find_all(True):
+    if child.has_attr('href'):
+        del child['href']
+print( tableIWant )
